56e87b823ecacc509927f59686c19e1f450ae7b1
Savan Visalpara <vsavan7@gmail.com>
2016-03-08 12:26:19 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="assets/demo.css">
+  <link rel="stylesheet" href="assets/header-fixed.css">
+  <link href='http://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" src="/assets/header-fixed.css">
+</head>
+<body>
+
+<header class="header-fixed">
+
+  <div class="header-limiter">
+
+    <h1><a href="index.php">student<span>record</span></h1></a>
+    <link rel="icon" type="image/png" href="icon.jpg" sizes="16x16">
+
+    <nav>
+      <a href="index.php">Home</a>
+      <a href="http://jivantshixan.blogspot.in/" target="_blank">Blog</a>
+      <a href="#">About</a>
+      <a href="statistics.html">Statistics</a>
+      <a href="activity.php">Activity</a>
+
+    </nav>
+
+  </div>
+
+</header>
+
+</body>
+</html>
