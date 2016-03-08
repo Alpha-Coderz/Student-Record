d1e272e7d8c7f0d5ea576948cbe9eaca0d495e50
Savan Visalpara <vsavan7@gmail.com>
2016-03-08 12:27:28 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+ <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <style>
+   .foot { margin-top:20px; padding-top:20px; padding-bottom:20px; background-color:#292c2f; color:white; width:100%; position:absolute;} 
+   
+
+  </style>
+
+
+  </head>
+  <body>
+  	<div class="container-fluid foot text-center">
+  <p> &copy; Students Records - Gujarat </p>
+</div>
+  </body>
+  </html>
