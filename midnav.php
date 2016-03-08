a933af4336b65153b51f42a624e36ed1f4933759
Savan Visalpara <vsavan7@gmail.com>
2016-03-08 12:31:05 +0530

    Create midnav.php

---
+++ midnav.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<style>
+
+
+
+</style>
+
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    
+    <div>
+      <ul class="nav navbar-nav">
+        
+        <li class="active"><a href="#">Profile</a></li>
+   
+        <li><a href="profile.php"> Dis-Abilities</a></li>
+        <li><a href="efforts.php"> Efforts </a></li>
+        <li><a href="advice.php"> Advice </a></li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+  
+
+</body>
+</html>
