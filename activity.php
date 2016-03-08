e36c6b7bbecaefa4f14789694d725498fe4a2876
Savan Visalpara <vsavan7@gmail.com>
2016-03-08 13:13:35 +0530

    Create activity.php

---
+++ activity.php
@@ -0,0 +1,67 @@
+<?php
+
+include 'header.php';
+?>
+<div class="container-fluid text-center">
+<p> <br><br>Here is a list of activity which dis-abled students should do ..<br></p>
+</div>
+<table class="table table-striped table-bordered">
+
+<tbody>
+<tr>
+
+<th>Dis-Ability </th>
+<th> Activity </th>
+
+</tr>
+
+</tbody>
+
+
+<tbody>
+<tr>
+
+<td> Basic Activity </td>
+<td> 1 - Making New Friends <br> 2 - Learning Independence <br> 3 - Increasing aerobic activity <br> 4 - Participating on a team </td>
+
+</tr>
+
+<tbody>
+<tr>
+
+<td>Blindness </td>
+<td> 1 - Cycling<br>2 - Swimming<br> 3 - Sports<br>4 - Making New Friends </td>
+
+</tr>
+
+</tbody>
+
+<tbody>
+<tr>
+
+<td>Deafness </td>
+<td> 1 - Pretend Play <br> 2 - Classroom Responsibility <br> 3 - Story play <br> 4 - Music</td>
+
+</tr>
+
+</tbody>
+
+<tbody>
+<tr>
+
+<td>Dumbness</td>
+<td> 1 - Creating Art <br> 2 - Yoga for peace <br> 3 - Teamwork <br> 4 - Healthy Eating </td>
+
+</tr>
+
+</tbody>
+
+
+</tbody>
+
+
+</table>
+<?php
+
+include 'footer.php';
+?>
